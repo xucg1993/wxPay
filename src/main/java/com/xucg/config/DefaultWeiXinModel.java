27019702034ccc231b8b3a4eea0463e8862926e2
xucg <80569287@qq.com>
2018-07-31 16:44:06 +0800

    新增 刷卡支付接口

--- src/main/java/com/xucg/config/DefaultWeiXinModel.java
+++ src/main/java/com/xucg/config/DefaultWeiXinModel.java
@@ -10,9 +10,6 @@ import com.xucg.model.WxPayPrePayModel;
  * @since 2017/11/8
  */
 public class DefaultWeiXinModel {
-    private static final String appId = "wxb24d7e2ad00d103b";
-    private static final String mchId = "1502029451";
-    private static final String payKey = "WRYEC0EgrjGdbxU5AVZBRpYQ7r95Dmc9";
     /**
      * 默认配置
      *
